48a8110c4265f0c29bfbba49d13f0d90378e38f4
norbcodes <thenorbert2008@gmail.com>
2024-08-27 12:29:15 +0200

    MacOS support (prob not working) and tips and tricks section
    Closes #17 and #27

---
+++ entities2/src/MacOS/sleep.cpp
@@ -0,0 +1,14 @@
+// entities2 © 2024 by norbcodes is licensed under CC BY-NC 4.0
+
+#include <cstdint>
+
+#include <unistd.h>
+
+/*
+I dunno if this works
+*/
+
+void SleepSeconds(uint32_t seconds)
+{
+    sleep(seconds);
+}
